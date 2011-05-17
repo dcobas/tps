04b120f32a6b24b799be16b16bf25e2bc54e3703
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-05-17 17:53:29 +0200

    add parser for arbitrary sequence execution
    supports normal sequencing, ranges, loops and nesting

---
+++ parse.py
@@ -0,0 +1,61 @@
+#!   /usr/bin/env   python
+#    coding: utf8
+
+import sys
+from ply import lex, yacc
+
+tokens = (
+    'NUMBER',
+    'REPEAT',
+    'LPAREN',
+    'RPAREN',
+    'DASH',
+)
+
+t_NUMBER = r'[0-9]+'
+t_REPEAT = r'repeat'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_DASH   = r'-'
+
+t_ignore = ' \t'
+
+def t_error(t):
+    print 'Illegal character [%s]' % t.value[0]
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+def p_battery_sequence(p):
+    '''battery : batch
+               | batch battery'''
+    if len(p) == 2:
+        p[0] = [ p[1] ]
+    else:
+        p[0] = [ p[1] ] + p[2]
+
+def p_batch_single(p):
+    'batch : tst'
+    p[0] = p[1]
+
+def p_batch_loop(p):
+    'batch : REPEAT NUMBER LPAREN battery RPAREN'
+    p[0] = [ 'loop', p[2] ] + p[4]
+
+def p_batch_range(p):
+    'batch : tst DASH tst'
+    p[0] = [ 'range', p[1], p[3] ]
+
+def p_tst(p):
+    'tst : NUMBER'
+    p[0] = p[1]
+
+def p_error(p):
+    print 'syntax error!'
+
+parser = yacc.yacc()
+
+if __name__ == '__main__':
+    result = parser.parse(sys.argv[1])
+    print result
+
