ee2f9f52bbe9711e24601d23472280cf9082e139
Samuel Iglesias Gonsalvez <samuel.iglesias.gonsalvez@cern.ch>
2011-05-31 11:37:00 +0200

    tps: added serial number input to tps.sh

--- tps.sh
+++ tps.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-rm /tmp/tps*
+rm -fr /tmp/tps*
 
-time ./tps.py -b SPEC -s $1 -t./test/spec/python -l /tmp 00 01 02 03 05 06 07 08
+serial=$1
+if [ x$1 == x"" ]; then
+	echo -n "Please, input SERIAL number: "
+        read serial
+fi
+
+time ./tps.py -b SPEC -s $serial -t./test/spec/python -l /tmp 00 01 02 03 05 06 07 08
+
