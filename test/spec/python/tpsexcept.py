efe5bdd5f065545c1ec8a0e1d52b2cf97a2c74b4
Samuel Iglesias Gonsalvez <samuel.iglesias.gonsalvez@cern.ch>
2011-06-01 08:21:44 +0200

    tps/test: added needed files to run stand-alone the tests for the SPEC board.

---
+++ test/spec/python/tpsexcept.py
@@ -0,0 +1,35 @@
+#!   /usr/bin/env   python
+#    coding: utf8
+
+class TpsException(Exception):
+    pass
+
+class TpsCritical(TpsException):
+    """critical error, abort the whole test suite"""
+    pass
+
+class TpsError(TpsException):
+    """error, continue remaining tests in test suite"""
+    pass
+
+class TpsUser(TpsException):
+    """error, user intervention required"""
+    pass
+
+class TpsWarning(TpsException):
+    """warning, a cautionary message should be displayed"""
+    pass
+
+class TpsInvalid(TpsException):
+    """reserved: invalid parameters"""
+
+class TpsNoBatch(TpsInvalid):
+    """reserved: a suite was created without batch of tests to run"""
+    pass
+
+class TpsBadTestNo(TpsInvalid):
+    """reserved: a bad test number was given"""
+    pass
+
+if __name__ == '__main__':
+    pass
